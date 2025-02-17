7035d55be7e9623d01bbc89394e40fc981c34afe
infernal3 <infernal1559@gmail.com>
2025-02-16 19:20:02 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 (function(){
-  function main(){
+  var U1Scaling=function U1Scaling(x){
+    if(typeof(x)!=='number'||!isFinite(x)||x<0)return 0;
+  }
+  var main=function main(){
     // TODO
   }
   main();
