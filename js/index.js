8ebecf12c84ebe867bb332e764ca7613ed8f0e68
infernal3 <infernal1559@gmail.com>
2025-02-18 23:04:39 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 (function(){
+  var el=E=>document.getElementById(E);
   var U1Scaling=function U1Scaling(x){
     if(typeof(x)!=='number'||!isFinite(x)||x<0)return 0;
     return void 0;
   }
-  var main=function main(){
+  var L=new Map([["Points",".UG9pbnR"],["Round",".Um91bmQ"],["U",".VXBncmF"],["O",".T3B0aW9"],[void 0,".VW5kZWZ"]]);
+  var setupHTML=function setupHTML(){
     var app=document.createElement("div"),div1=document.createElement("div"),div2=document.createElement("div");
     app.id="app";
     div1.id="idleData";
     div2.id="choices";
-    div1.innerHTML=`Idle Poll, Round #1.<br>You have 10 Points.<br>Options:<br>[O1] +100 Points<br>[O2] x10 Points`;
+    div1.innerHTML=`Idle Poll, Round #<span id="round">NaN</span>.<br>You have <span id="points">NaN</span> Points.<br>Options:<br>[O1] +100 Points<br>[O2] x10 Points`;
     app.append(div1);
     var click1=document.createElement("button");
     click1.id="click1";
@@ -17,6 +19,21 @@
     div2.append(click1);
     app.append(div2);
     document.body.append(app);
+  }
+  var updateHTML=function updateHTML(){
+    el("round").textContent=globalThis.Data[L.get("Round")];
+    el("points").textContent=globalThis.Data[L.get("Points")];
+  }
+  var setupData=function setupData(){
+    //TODO: Implement Local Storage
+    var Data={};
+    L.forEach((v,k)=>{Data[v]=undefined;});
+    Data[L.get("Points")]=10;
+    return Data;
+  }
+  var main=function main(){
+    setupHTML();
+    globalThis.Data=setupData();
   }
   main();
 }).call(this);
