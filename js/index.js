1e17a363b6ac857418d10072573765f023a97be7
infernal3 <infernal1559@gmail.com>
2025-02-19 18:28:52 -0600

    Update index.js
    Partially implement U1, kind of. The logic is not there yet.

--- js/index.js
+++ js/index.js
@@ -17,13 +17,33 @@
     if(typeof(x)!=='number'||!isFinite(x)||x<0)return 0;
     return void 0;
   }
+  // Data point obfuscation! No idea why I did this, it just exists.
+  // Yes, it's confusing. Who cares?
   var L=new Map([["Points",".UG9pbnR"],["Round",".Um91bmQ"],["Upgrade",".VXBncmF"],["Option",".T3B0aW9"],[void 0,".VW5kZWZ"],["Last",".VGltZXI"]]);
+  
+  var createButton=function createButton(id,name){
+    var temp=document.createElement("button");
+    temp.id=id;
+    temp.textContent=`[${name}]`;
+    temp.addEventListener("click",new Function(`HandleAction("${name}")`));
+    return temp;
+  }
   var setupHTML=function setupHTML(){
+    // This function is called once, when the page is being set up.
+    // Requires DOM content to be loaded.
+    // Creates and sets up the HTML page.
     var app=document.createElement("div"),div1=document.createElement("div"),div2=document.createElement("div"),div3=document.createElement("div");
     app.id="app";
     div1.id="idleData";
     div2.id="choices";
-    div1.innerHTML=`Idle Poll, Round #<span id="round">[Error]</span>.<br>You have <span id="points">[Error]</span> Points.<br><br>Options:<br><span id="O1">[O1] +<span id="O1Effect">100</span> Points</span><br><span id="O2">[O2] x<span id="O2Effect">10</span> Points</span>`;
+    div1.innerHTML=`
+    Idle Poll, Round #<span id="round">1</span>.
+    You have <span id="points">10</span> Points.<br>
+    Options:
+    <span id="O1">[O1] +<span id="O1Effect">100</span> Points</span>
+    <span id="O2">[O2] x<span id="O2Effect">10</span> Points</span><br>
+    Upgrades:
+    <span id="U1">[U1] Multiply O2's effect by x<span id="U1Effect">1000</span>.</span><br>`;
     app.append(div1);
     var click1=document.createElement("button"),click2=document.createElement("button");
     click1.id="click1";
@@ -38,19 +58,25 @@
     });
     div2.append(click1);
     div2.append(click2);
+    div2.append(createButton("click11","U1"));
     div3.id="delay";
     div2.append(div3);
     app.append(div2);
     document.body.append(app);
   }
   var updateHTML=function updateHTML(){
+    // This function is called very often.
+    // Updates some HTML stuff.
     el("round").textContent=globalThis.Data[L.get("Round")];
     el("points").textContent=globalThis.Data[L.get("Points")];
     el("O1Effect").textContent=globalThis.Data[L.get("Option")][1];
     el("O2Effect").textContent=globalThis.Data[L.get("Option")][2];
   }
   var setupData=function setupData(){
-    //TODO: Implement Local Storage
+    // This function is called once, when the page is being set up.
+    // Creates a DATA object to hold all data.
+    
+    // TODO: Implement Local Storage
     var Data={},obj1=[void 0,100,10];
     L.forEach((v,k)=>{Data[v]=undefined;});
     Data[L.get("Option")]=obj1;
@@ -72,6 +98,9 @@
       case "O2":
         O2();
         break;
+      case "U1":
+        U1();
+        break;
       default:
         console.log(`Error: Action ${action} does not exist.`);
         break;
@@ -85,6 +114,9 @@
   }
   var O2=function O2(){
     Data[L.get("Points")]*=globalThis.Data[L.get("Option")][2];//TODO: Implement break_eternity.js to prevent numeric overflow
+  }
+  var U1=function U1(){
+    console.log("not implemented");
   }
   var main=function main(){
     setupHTML();
