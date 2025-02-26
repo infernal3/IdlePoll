5e7cc07a03ef9cd9b01c4ec96565b84f206e7913
infernal3 <infernal1559@gmail.com>
2025-02-25 19:15:08 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -81,7 +81,7 @@
     // Modifies some aspects of the page that are data-sensitive.
     if(Data[L.get("Upgrade")][1])el("U1-extra").textContent="BOUGHT";
     if(Data[L.get("Upgrade")][2]){
-      el("U2").childNodes[1].textContent="O3 is unlocked";
+      el("U2").childNodes[1].textContent=" O3 is unlocked";
       el("U2-extra").textContent="BOUGHT";
     }
     if(Data[L.get("Upgrade")][3]){
@@ -241,7 +241,7 @@
     if(Data[L.get("Upgrade")][2])return "U2 already bought";
     if(Data[L.get("Points")].lt(1e10))return "Insufficient Points: Need 1e10";
     el("U2-extra").textContent="BOUGHT";
-    el("U2").childNodes[1].textContent="O3 is unlocked";
+    el("U2").childNodes[1].textContent=" O3 is unlocked";
     Data[L.get("Points")]=Data[L.get("Points")].sub(1e10);
     Data[L.get("Upgrade")][2]=1;
     el('click3').style=Data[L.get("Upgrade")][2]?"":"display:none;"
