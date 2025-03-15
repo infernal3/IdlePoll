c61f965bb9d1bbc4ba155d9c25583e6cf7e6a783
infernal3 <infernal1559@gmail.com>
2025-03-14 23:01:00 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -216,6 +216,11 @@
       return;
     }
     if(debugMode)console.log("[IdlePoll:Debug] Imported save.");
+    Data=parseDecimalData(Data,"Points");
+    Data=parseDecimalData(Data,"Option",1);
+    Data=parseDecimalData(Data,"Option",2);
+    if(!"number"==typeof Data[L.get("Upgrade")][1])Data[L.get("Upgrade")][1]=Number(Data[L.get("Upgrade")][1]);
+    if(isNaN(Data[L.get("Upgrade")][1]))Data[L.get("Upgrade")][1]=0;
     Data=JSON.parse(atob(data));
   }
   var ImportClipboard=async function ImportClipboard(){
